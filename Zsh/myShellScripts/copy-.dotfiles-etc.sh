fa6076f6bd958842f2248947810098c9a68875a1
Steve <binarysmurf@users.noreply.github.com>
2022-06-21 21:15:25 +0800

    Additional scripts

---
+++ Zsh/myShellScripts/copy-.dotfiles-etc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Copy .dotFiles etc
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Copy configs etc
+# @raycast.author Steve
+
+cp ~/.config/starship.toml ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+cp ~/.exrc ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+cp ~/.steve_aliases.sh ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+cp ~/.zshrc ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+cp ~/.steve_functions.sh ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+cp ~/.zprofile ~/Geekery/Github/CodingAdventures/Zsh/myDotFiles
+open -a "Github Desktop"
