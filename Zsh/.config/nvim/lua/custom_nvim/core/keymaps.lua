675223432ade92ad2c8ddcca2d9d9dd0de2a8174
Steve <binarysmurf@users.noreply.github.com>
2023-01-21 18:55:18 +0800

    alt key like a motherfucker!

--- Zsh/.config/nvim/lua/custom_nvim/core/keymaps.lua
+++ Zsh/.config/nvim/lua/custom_nvim/core/keymaps.lua
@@ -2,28 +2,27 @@ vim.g.mapleader = " "
 
 local key = vim.keymap
 
+key.set("n", "<F1>", ":NvimTreeToggle<CR>", { silent = true })
 key.set("n", "<C-s>", ":w<CR>")
-key.set("n", "<C-w>", ":so %<CR>", { silent = true })
 key.set("n", "<C-q>", ":q<CR>")
-key.set("n", "<C-z>", ":qall!<CR>")
+key.set("n", "<A-q>", ":qall!<CR>")
 key.set("n", "<C-x>", ":q!<CR>")
-key.set("n", "<leader>X", ":qa!<CR>")
 key.set("n", "<F7>", ":close<CR>")
 key.set("n", "<F8>", ":nohl<CR>", { silent = true })
 key.set('n', ",", "$p")
 
 key.set("n", "<leader>sh", "<cmd>split<CR>")
 key.set("n", "<leader>sv", "<cmd>vsplit<CR>")
-key.set("n", "<F6>", "<cmd>split<CR>")
 key.set("n", "<F5>", "<cmd>vsplit<CR>")
+key.set("n", "<F6>", "<cmd>split<CR>")
 
 key.set("n", "<C-y>", "<cmd>Yapf<CR>")
-key.set("n", "<leader>Y", ":Yapf<cr>")
+key.set("n", "<leader>y", ":Yapf<cr>")
 
-key.set("n", "<C-t>", ":tabnew<CR>")
-key.set("n", "<leader>x", ":tabclose<CR>")
-key.set("n", "<C-]>", ":tabn<CR>")
-key.set("n", "<F1>", ":NvimTreeToggle<CR>", { silent = true })
+key.set("n", "<A-t>", ":tabnew<CR>")
+key.set("n", "<A-x>", ":tabclose<CR>")
+key.set("n", "<A-]>", ":tabn<CR>")
+key.set("n", "<A-[>", ":tabp<CR>")
 
 key.set("n", "<F9>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 key.set("n", "<F10>", "<cmd>Telescope oldfiles<cr>") -- list open buffers in current neovim instance
